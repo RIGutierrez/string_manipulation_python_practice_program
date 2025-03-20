91741cd5208ce0b9c515939b44e29843a7d30553
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 15:51:47 +0800

    Program 2

--- prog_02_print_num_6_digit_format.py
+++ prog_02_print_num_6_digit_format.py
@@ -1,5 +1,16 @@
-# ask user to input numbers
-user_input = str(input("Please input a number (0-1000): "))
+while True:
+    try:
+        # ask user to input numbers
+        user_input = int(input("Please input a number (0-1000): "))
+        
+        # check if number is 0-1000 range
+        if 0 <= user_input <= 1000:
+            break
+        else:
+            print("Input must be from 0-1000")
+    
+    except ValueError:
+        print("Please input valid number")
 
 # add zero at the beginning
-print(user_input.zfill(6))
+print(str(user_input).zfill(6))
