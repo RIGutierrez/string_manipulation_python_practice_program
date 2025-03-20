6c9dc2c41060af432584ae3c6fffa37f10a0e59a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 17:04:30 +0800

    Program 4

--- prog_04_print_name_in_lower_case_letter.py
+++ prog_04_print_name_in_lower_case_letter.py
@@ -2,7 +2,7 @@ while True:
     # ask user to input their fullname
     name = input("Please input your fullname: ").strip()
 
-    # check input if valid
+    # check input if invalid
     if not name:
         print("Invalid input. Try again.")
         continue
