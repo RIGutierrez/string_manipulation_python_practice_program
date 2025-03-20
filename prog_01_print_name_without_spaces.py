2078755d17d4574ae7c1c23a73280b2f96796b98
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 21:08:23 +0800

    Program 1 Final

--- prog_01_print_name_without_spaces.py
+++ prog_01_print_name_without_spaces.py
@@ -1,6 +1,6 @@
 while True:
     # ask user to input their name
-    name = str(input("Please input your fullname (with spaces at the beginning): "))
+    name = input("Please input your fullname (with spaces at the beginning): ")
 
     # check input if have spaces
     if name.startswith(" "):
