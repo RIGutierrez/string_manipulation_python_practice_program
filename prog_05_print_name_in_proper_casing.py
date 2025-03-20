ebb88f7d33769d840b349dc1279ada913003c4f7
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 16:51:08 +0800

    try if working

--- prog_05_print_name_in_proper_casing.py
+++ prog_05_print_name_in_proper_casing.py
@@ -1,3 +1,8 @@
-# ask user to input their full name
+while True:
+    # ask user to input their full name
+    name = input("Please input your fullname: ").strip()
+
+    break
+print(name.title())
 # check input if invalid
 # print in proper casing
