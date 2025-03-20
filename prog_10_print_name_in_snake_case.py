4164c85da92a20e1b3611f2576ed52463269dfbd
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 21:50:35 +0800

    strip lower replace

--- prog_10_print_name_in_snake_case.py
+++ prog_10_print_name_in_snake_case.py
@@ -1,3 +1,9 @@
+name = input("Name: ")
+
+name_snake = name.strip().lower().replace(" ", "_")
+
+print(name_snake)
+
 # ask user to input their full name
 # check input if invalid
 # print in snake case
