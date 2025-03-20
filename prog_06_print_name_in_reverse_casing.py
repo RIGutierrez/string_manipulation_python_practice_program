ded5596de21d3eeecc380c2c6dc6c10b9703d736
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 17:14:37 +0800

    Program 6

--- prog_06_print_name_in_reverse_casing.py
+++ prog_06_print_name_in_reverse_casing.py
@@ -1,3 +1,13 @@
-# ask user to input their full name
-# check input if invalid
+while True:
+    # ask user to input their full name
+    name = input("Please input your fullname in incorrect casing: ")
+    
+    # check input if invalid
+    if not name:
+        print("Invalid input. Try again.")
+        continue
+
+    break
+
 # print in reverse casing
+print(name.swapcase())
