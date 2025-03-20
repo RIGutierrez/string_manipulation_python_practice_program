9deb752f869b6740a98eed4d21e8c1e8d46362e7
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 21:31:28 +0800

    strip title replace

--- prog_09_print_name_in_pascal_case.py
+++ prog_09_print_name_in_pascal_case.py
@@ -1,3 +1,9 @@
-# ask use to input their full name 
+name = input("name: ")
+
+name_pascal = name.strip().title().replace(" ", "")
+
+print(name_pascal)
+
+# ask user to input their full name 
 # check input if invalid
 # print in pascal case
