f5f25460137938fbf213c93766f22cff82b752c0
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 22:04:35 +0800

    Program 7

--- prog_07_print_num_of_words_input.py
+++ prog_07_print_num_of_words_input.py
@@ -1,6 +1,6 @@
 while True:
     # ask user for input
-    input_statement = input("Statement: ")
+    input_statement = input("Statement: ").strip()
     
     # check input if invalid
     if not input_statement:
