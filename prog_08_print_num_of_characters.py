5c09d808981acd9818c05d64208c5c93ef7a4ccf
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 20:39:20 +0800

    check len if working

--- prog_08_print_num_of_characters.py
+++ prog_08_print_num_of_characters.py
@@ -1,3 +1,9 @@
+while True:
+    name = input("Please input your fullname: ").strip()
+
+    break
+print(len(name))
+
 # ask user to input their fullname
 # check input if invalid
 # print number of characters
